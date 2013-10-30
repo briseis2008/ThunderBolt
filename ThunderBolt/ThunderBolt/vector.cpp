e696de04f351cadb762d1073ccc7bcc7a38c4169
migorain <migocjj@gmail.com>
2013-10-30 14:40:45 -0400

    New files

---
+++ ThunderBolt/ThunderBolt/vector.cpp
@@ -0,0 +1,17 @@
+#include "vector.h"
+
+const Vector Vector::operator+ (const Vector &rhs) const {
+    return Vector(x + rhs.x, y + rhs.y);
+}
+
+const Vector Vector::operator- (const Vector &rhs) const {
+    return Vector(x - rhs.x, y - rhs.y);
+}
+
+const Vector Vector::operator* (const double rhs) const {
+    return Vector(x * rhs, y * rhs);
+}
+const Vector Vector::operator/ (const double rhs) const {
+    if (rhs == 0) return *this;
+    return Vector(x / rhs, y / rhs);
+}
