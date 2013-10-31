2ce65b34c183f20b2522aa4252ac7a14d7ef7827
Yi <yigu@andrew.cmu.edu>
2013-10-31 18:55:42 -0400

    plane added

---
+++ ThunderBolt/ThunderBolt/Plane.cpp
@@ -0,0 +1,9 @@
+//
+//  Plane.cpp
+//  ThunderBolt
+//
+//  Created by yigu on 10/31/13.
+//  Copyright (c) 2013 CMU. All rights reserved.
+//
+
+#include "Plane.h"
---
+++ ThunderBolt/ThunderBolt/Plane.h
@@ -0,0 +1,10 @@
+#ifndef __ThunderBolt__Plane__
+#define __ThunderBolt__Plane__
+
+#include <stdio.h>
+#include <math.h>
+#include "vector.h"
+#include "fssimplewindow.h"
+#include "ysglfontdata.h"
+
+#endif /* defined(__ThunderBolt__Plane__) */
