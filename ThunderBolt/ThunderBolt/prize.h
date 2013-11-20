51a00d88b3c1dabc2ba1c720d0d315773a1c7103
briseis2008 <rebeccayanzhang@gmail.com>
2013-11-20 15:18:32 -0500

    Update prize.h

--- ThunderBolt/ThunderBolt/prize.h
+++ ThunderBolt/ThunderBolt/prize.h
@@ -1,13 +1,50 @@
-#ifndef THUNDERBOLT_PRIZE_H
-#define THUNDERBOLT_PRIZE_H
+/*
+ Thunderbolt prize.h edition 2
+ Created by Yan Zhang
+ Andrew ID: yanz3
+ */
+#ifndef ThunderBolt_Prize_h
+#define ThunderBolt_Prize_h
 
-#include "plane.h"
+#include <stdio.h>
+#include <math.h>
+#include "fssimplewindow.h"
+#include "ysglfontdata.h"
+#include "vector.h"
+#include "color.h"
+#include "global.h"
+#include "linked_list.h"
+#include "missile.h"
 
-class WeaponUpdate : public Plane {
-    MissileType type;
-    
-public:
-    WeaponUpdate(MissileType type) : type(type) {};
+enum PrizeType
+{
+    LIFE
 };
 
+class Prize
+{
+    
+protected:
+    
+    int nprize;
+    int p_sizex, p_sizey;
+    Vector2 position;
+    Vector2 velocity;
+    PrizeType type;
+    bool c;
+    
+public:
+    int p_x0;
+    int prize_lifetime;
+    int prize_state;
+    Prize();
+    void Initialized();
+    virtual void Draw();
+    virtual void Move();
+    //virtual int CheckCatchByPlane(Plane &plane);
+    virtual void Disappear(void);
+    //virtual void setType(MissileType &type);
+    Prize getType() const;
+
+};
 #endif
